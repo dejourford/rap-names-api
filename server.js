fb04623bce69bef604ea4155a4f147927b843e18
De'Jour Ford <dejourford@my.unt.edu>
2023-02-27 14:08:38 -0600

    add get function for index, css, and js to server

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 8
 
+
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/styles.css', (request, response) => {
+    response.sendFile(__dirname + '/styles.css')
+})
+
+app.get('/main.js', (request, response) => {
+    response.sendFile(__dirname + '/main.js')
 })
 
 app.listen(PORT, () => {
