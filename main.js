f70e266f4592125e3fbd5ce43c3263d0076210bf
De'Jour Ford <dejourford@my.unt.edu>
2023-02-28 20:25:37 -0600

    changed api links to fetch one item instead of all

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const rapperName = document.querySelector('#rapperName').value
+
 const searchBtn = document.querySelector('#searchBtn')
 const birthName = document.querySelector('#birthName')
 
-async function getName (rapperName)  {
-    const response = await fetch(`http://localhost:8/api/:${rapperName}`)
+async function getName ()  {
+    const rapperName = document.querySelector('#rapperName').value
+    const response = await fetch(`http://localhost:8/api/${rapperName}`)
     const data = await response.json()
-    console.log('function working')
+    
     console.log(data)
-    birthName.textContent = 'hello world'
-    console.log('wtf')
+    birthName.textContent = data.birthName
+    
 }
 
 searchBtn.addEventListener('click', ()=> {
     getName(rapperName)
-    console.log('clicked')
+    
 })
